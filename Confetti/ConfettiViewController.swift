dd4c8915a808a53e6514df87137817279f2bf50d
Julia Nikitina <julianikitina.ios@gmail.com>
2018-12-16 00:10:28 +0300

    Clean up

--- Confetti/ConfettiViewController.swift
+++ Confetti/ConfettiViewController.swift
@@ -11,51 +11,36 @@ import UIKit
 final class ConfettiViewController: UIViewController {
 
     private var emitterLayer: CAEmitterLayer?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         createEmitterLayer()
     }
-
     private func createEmitterLayer() {
         emitterLayer = CAEmitterLayer()
-        
         let purpleCell = CAEmitterCell()
         purpleCell.contents = UIImage.purpleConfetti?.cgImage
-        
         let pinkCell = CAEmitterCell()
         pinkCell.contents = UIImage.pinkConfetti?.cgImage
-        
         let yellowCell = CAEmitterCell()
         yellowCell.contents = UIImage.yellowConfetti?.cgImage
-        
         var dotCells = [CAEmitterCell]()
-        
         let images = [
             UIImage.pinkDot?.cgImage,
             UIImage.greenDot?.cgImage,
             UIImage.yellowDot?.cgImage
         ]
-        
         for index in 0..<images.count {
-            
             let cell = CAEmitterCell()
             cell.contents = images[index]
-            
             cell.birthRate = 10
             cell.lifetime = 100
             cell.velocity = 150
             cell.scale = 0.1
-            
             cell.emissionRange = CGFloat.pi / 2
             cell.emissionLongitude = CGFloat.pi
-            
             dotCells.append(cell)
-            
         }
-        
         var cells = [purpleCell, pinkCell, yellowCell]
-        
         cells.forEach { cell in
             cell.birthRate = 10
             cell.lifetime = 100
@@ -63,19 +48,16 @@ final class ConfettiViewController: UIViewController {
             cell.spin = 4
             cell.spinRange = 10.0
             cell.scale = 0.04
-            
             cell.emissionRange = CGFloat.pi / 2
             cell.emissionLongitude = CGFloat.pi
-            
         }
-        
         cells += dotCells
         emitterLayer?.emitterCells = cells
-        
         emitterLayer?.emitterShape = .line
-        emitterLayer?.emitterPosition = CGPoint(x: view.frame.width / 2, y: 25)
-        emitterLayer?.emitterSize = CGSize(width: view.frame.width, height: 1)
-        
+        emitterLayer?.emitterPosition = CGPoint(x: view.frame.width / 2,
+                                                y: 25)
+        emitterLayer?.emitterSize = CGSize(width: view.frame.width,
+                                           height: 1)
         view.layer.addSublayer(emitterLayer ?? CAEmitterLayer())
     }
 }
--- Confetti/UIImage.swift
+++ Confetti/UIImage.swift
@@ -11,32 +11,38 @@ import UIKit
 extension UIImage {
     
     static var purpleConfetti: UIImage? {
-        guard let purple = UIImage(named: "purple_confetti.png") else { return nil }
+        guard let purple = UIImage(named: "purple_confetti.png")
+            else { return nil }
         return purple
     }
     
     static var yellowConfetti: UIImage? {
-        guard let yellow = UIImage(named: "yellow_confetti.png") else { return nil }
+        guard let yellow = UIImage(named: "yellow_confetti.png")
+            else { return nil }
         return yellow
     }
     
     static var pinkConfetti: UIImage? {
-        guard let pink = UIImage(named: "pink_confetti.png") else { return nil }
+        guard let pink = UIImage(named: "pink_confetti.png")
+            else { return nil }
         return pink
     }
     
     static var pinkDot: UIImage? {
-        guard let pink = UIImage(named: "pink_dot.png") else { return nil }
+        guard let pink = UIImage(named: "pink_dot.png")
+            else { return nil }
         return pink
     }
     
     static var greenDot: UIImage? {
-        guard let green = UIImage(named: "green_dot.png") else { return nil }
+        guard let green = UIImage(named: "green_dot.png")
+            else { return nil }
         return green
     }
     
     static var yellowDot: UIImage? {
-        guard let yellow = UIImage(named: "yellow_dot.png") else { return nil }
+        guard let yellow = UIImage(named: "yellow_dot.png")
+            else { return nil }
         return yellow
     }
 }
